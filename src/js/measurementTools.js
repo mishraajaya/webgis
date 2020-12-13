840f5fdee75ea776b565d6559bf7fca60db6ebef
Ajaya Mishra <azaya.mishra@gmail.com>
2020-12-13 11:56:34 +0900

    measurement tool continue

--- src/js/measurementTools.js
+++ src/js/measurementTools.js
@@ -1,6 +1,7 @@
 import LineString from 'ol/geom/LineString'
 import Draw from 'ol/interaction/Draw'
 import {getLength} from 'ol/sphere'
+import Overlay from 'ol/Overlay'
 
 const measurementToolsMenu = document.getElementById('measurementToolsMenu')
 
@@ -25,6 +26,54 @@ document.onreadystatechange = function () {
     let draw = ''
     let sketch = ''
 
+    let measurementTooltipElement = ''
+    let measurementTooltip = ''
+
+    let helpTooltipElement = ''
+    let helpTooltip = ''
+    let continueDrawLineMsg = 'Click to continue drawing Line'
+
+    const pointerMoveHandler = e => {
+      if (e.dragging) return
+
+      let helpMessage = "Click to start Drawing"
+
+      if (sketch) {
+        helpMessage = continueDrawLineMsg
+      }
+
+      helpTooltipElement.innerHTML = helpMessage
+      helpTooltip.setPosition(e.coordinate)
+      helpTooltipElement.classList.remove('hidden')
+    }
+
+    const createHelpTooltip = () => {
+      helpTooltipElement = document.createElement('div')
+      helpTooltipElement.className = 'ol-tooltip hidden'
+      helpTooltip = new Overlay({
+        element: helpTooltipElement,
+        offset: [15, 0],
+        positioning: 'center-left'
+      })
+
+      map.addOverlay(helpTooltip)
+    }
+
+    const createMeasurementTooltip = () => {
+      if (measurementTooltipElement) {
+        measurementTooltipElement.parentNode.removeChild(measurementTooltipElement)
+      }
+      measurementTooltipElement = document.createElement('div')
+      measurementTooltipElement.className = 'ol-tooltip ol-tooltip-measure'
+
+      measurementTooltip = new Overlay({
+        element: measurementTooltipElement,
+        offset: [0, -15],
+        positioning: 'bottom-center'
+      })
+      map.addOverlay(measurementTooltip)
+    }
+
     const calculateLength = line => {
       const length = getLength(line)
       let output
@@ -46,20 +95,28 @@ document.onreadystatechange = function () {
         })
       } else return null
 
+      map.on('pointermove', pointerMoveHandler)
       map.addInteraction(draw)
+      createMeasurementTooltip()
+      createHelpTooltip()
 
       let listener = ''
 
       draw.on('drawstart', function(e) {
         sketch = e.feature
 
+        let tooltipCoordinate = e.coordinate
+
         listener = sketch.getGeometry().on('change', function(e) {
           const geom = e.target
           let output = ''
           if (geom instanceof LineString) {
             output = calculateLength(geom)
-            console.log("output=", output)
+            // console.log("output=", output)
+            tooltipCoordinate = geom.getLastCoordinate()
           }
+          measurementTooltipElement.innerHTML = output
+          measurementTooltip.setPosition(tooltipCoordinate)
         })
       })
 
